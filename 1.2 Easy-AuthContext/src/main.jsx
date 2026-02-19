d18a8f503cbbd99b56deb28d3a8e05c421bdabf2
suhaibkhan <johnxuhaib@gmail.com>
2026-02-19 10:16:07 +0530

    added testing

--- 1.2 Easy-AuthContext/src/main.jsx
+++ 1.2 Easy-AuthContext/src/main.jsx
@@ -4,9 +4,10 @@ import App from './App.jsx'
 import AuthProvider from './context/Auth.jsx'
 
 createRoot(document.getElementById('root')).render(
-
+<StrictMode>
 <AuthProvider>      
     <App />
-</AuthProvider>
+        </AuthProvider>
+        </StrictMode>
     
 )
